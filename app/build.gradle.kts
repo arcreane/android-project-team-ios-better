9932a4849205e74d7f05060f007ac3b74a18edcc
Nam Khan <namkhanh.le.vn@gmail.com>
2026-05-24 19:19:20 +0200

    fix: typos and dependencies updated. Tests run succesfully

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
 
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 }
--- app/src/main/java/com/example/aroundme/ui/list/EventAdapter.java
+++ app/src/main/java/com/example/aroundme/ui/list/EventAdapter.java
@@ -12,7 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.aroundme.R;
 import com.example.aroundme.model.Event;
-import com.example.bumptech.glide.Glide;
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
